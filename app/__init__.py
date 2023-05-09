5f42724e85e331996610c55cfda4398eff2df7b7
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-08 22:49:40 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
-def Create_app():
+def create_app():
     app = Flask(__name__)
 
     app.config['Secret Key'] = 'your-secret-key-here'
 
     from .routes.car import cars_bp
+    from .routes.brand import brand_bp
     app.register_blueprint(cars_bp)
+    app.register_blueprint(brand_bp)
 
     return app
