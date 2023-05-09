56ad36f96371df82529b6873b45789934788c06e
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-08 22:19:31 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template
 
 cars_bp = Blueprint('cars', __name__)
-manufacturers_bp = Blueprint('manufacturers', __name__)
 
 @cars_bp.route('/')
 def index():
     return render_template('cars/cars.html')
 
-@manufacturers_bp.route('/')
-def manu_index():
-    return render_template('manufacturers'/'manufacturers.html')
+@cars_bp.route('/list')
+def list_cars():
+    cars = ['Nissan', 'Toyota', 'Honda', 'Ford']
+    return render_template('cars/list.html', cars=cars)
+
+@cars_bp.route('/<string:car_name>')
+def car_details(car_name):
+    return f"This is the details page for {car_name}"
