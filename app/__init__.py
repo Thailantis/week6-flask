6fbcb20c2c7928c1948a80d0ebdc71a99692e707
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-08 22:25:59 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,7 @@ def Create_app():
 
     app.config['Secret Key'] = 'your-secret-key-here'
 
-    from app.cars.routes import cars_bp
+    from .routes.car import cars_bp
     app.register_blueprint(cars_bp)
-
-    from app.manufacturers.routes import manufacturers_bp
-    app.register_blueprint(manufacturers_bp)
 
     return app
