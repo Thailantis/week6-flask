8633d95caa256b07d24abded91db200690d8dd4a
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-10 00:26:31 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlaclehmy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 def create_app():
     app = Flask(__name__)
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
 
     app.config['Secret Key'] = 'your-secret-key-here'
 
