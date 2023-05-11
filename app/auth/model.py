32b1a8a06fe33b588a053c2950b173895a21ff81
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-10 23:55:19 -0400

    Create model.py

---
+++ app/auth/model.py
@@ -0,0 +1,14 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    cars = db.relationship('Car', backref='user', lazy=True) 
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(50), nullable=False)
+    model = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
